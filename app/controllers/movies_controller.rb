6753941807bc11e14bde38c626fd08f11691def6
Maciej Dobosz <dobosz.ma@interia.pl>
2020-03-03 19:38:41 +0100

    Task 2 - Add json API for movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,10 +3,35 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.all.decorate
+    respond_to do |format|
+      format.html
+      format.json {
+        movies = {}
+        movies['movies'] = @movies.map { |movie| { 'id': movie.id, 'title': movie.title } }
+        if params['genres']
+          genres, genre_names, genre_counts = [], {}, Hash.new(0)
+
+          @movies.each do |movie|
+            genre_counts[movie.genre.name] += 1
+            genre_names[movie.genre_id] = movie.genre.name
+          end
+
+          genre_names.each do |id, genre|
+            genres.push({'id': id, 'name': genre, 'total_count': genre_counts[genre]})
+          end
+          movies['genres'] = genres
+        end
+        render json: movies
+      }
+    end
   end
 
   def show
     @movie = Movie.find(params[:id]).decorate
+    respond_to do |format|
+      format.html
+      format.json { render json: @movie }
+    end
   end
 
   def send_info
--- spec/requests/movies_spec.rb
+++ spec/requests/movies_spec.rb
@@ -7,4 +7,16 @@ describe "Movies requests", type: :request do
       expect(page).to have_selector("h1", text: "Movies")
     end
   end
+
+  describe "movies json" do
+    it "displays movies" do
+      visit "/movies.json"
+      expect(page.body).to include('movies')
+    end
+
+    it "displays genres" do
+      visit "/movies.json?genres=true"
+      expect(page.body).to include('genres')
+    end
+  end
 end
